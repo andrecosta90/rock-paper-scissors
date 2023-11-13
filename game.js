613895b2ba5dcc9b7e6f794426b62456fe7ec712
André Costa <andrehesilva@gmail.com>
2023-11-13 18:12:37 -0300

    Add a function that plays a single round of RPS

--- game.js
+++ game.js
@@ -4,4 +4,38 @@ function getComputerChoice() {
     return options[selected_option];
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return `It's a tie! Both players throw ${playerSelection}!`
+    }
+
+    let bad_message = "You Lose!";
+    let good_message = "You Win!";
+
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            return `${bad_message} Paper beats Rock!`;
+        } else {
+            return `${good_message} Rock beats Scissors!`;
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            return `${bad_message} Scissors beats Paper!`;
+        } else {
+            return `${good_message} Paper beats Rock!`;
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            return `${bad_message} Rock beats Scissors!`;
+        } else {
+            return `${good_message} Scissors beats Paper!`;
+        }
+    }
+
+    console.error(`Invalid options = **${playerSelection}** ! You should choose between: rock, paper and scissors.`);
+}
+
 console.log("Hello World");
