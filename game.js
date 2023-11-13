75e18b229fabd697f1554b8419719b02c94385c5
André Costa <andrehesilva@gmail.com>
2023-11-13 18:41:41 -0300

    Add a function 'game' that keeps score and reports a winner or loser at the end

--- game.js
+++ game.js
@@ -9,33 +9,77 @@ function playRound(playerSelection, computerSelection) {
     computerSelection = computerSelection.toLowerCase();
 
     if (playerSelection === computerSelection) {
-        return `It's a tie! Both players throw ${playerSelection}!`
+        return 0;
     }
-
-    let bad_message = "You Lose!";
-    let good_message = "You Win!";
 
     if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
-            return `${bad_message} Paper beats Rock!`;
+            return -1;
         } else {
-            return `${good_message} Rock beats Scissors!`;
+            return 1;
         }
     } else if (playerSelection === 'paper') {
         if (computerSelection === 'scissors') {
-            return `${bad_message} Scissors beats Paper!`;
+            return -1;
         } else {
-            return `${good_message} Paper beats Rock!`;
+            return 1;
         }
     } else if (playerSelection === 'scissors') {
         if (computerSelection === 'rock') {
-            return `${bad_message} Rock beats Scissors!`;
+            return -1;
         } else {
-            return `${good_message} Scissors beats Paper!`;
+            return 1;
         }
     }
 
-    console.error(`Invalid options = **${playerSelection}** ! You should choose between: rock, paper and scissors.`);
+    console.error(`Invalid options = **${playerSelection}**!`);
+}
+
+function game() {
+
+    let player_score = 0;
+    let compute_score = 0;
+
+    for (let i = 1; i <= 5; i++) {
+        console.log(`game #${i}`);
+
+        let playerSelection = prompt("Rock, Paper or Scissors? ");
+        let computerSelection = getComputerChoice();
+
+
+        let result = playRound(playerSelection, computerSelection);
+
+        console.log(`option=${playerSelection}`);
+        console.log(`result=${result}`);
+        console.log(`playerSelection=${playerSelection}`);
+        console.log(`computerSelection=${computerSelection}`);
+
+        let message;
+
+        switch (result) {
+            case 1:
+                message = `You Win! ${playerSelection} beats ${computerSelection}!`;
+                player_score++;
+                break;
+            case -1:
+                message = `You Lose! ${computerSelection} beats ${playerSelection}!`
+                compute_score++;
+                break;
+            default:
+                message = `It's a tie! Both players throw ${playerSelection}!`;
+        }
+
+        console.log(message);
+        console.log(`Player Score = ${player_score}`);
+        console.log(`Computer Score = ${compute_score}`);
+    }
+
+    console.log(`Player ${player_score} X ${compute_score} Computer`);
+    console.log(
+        player_score > compute_score ? 'Player is the WINNER!' :
+        player_score < compute_score ? 'Computer is the WINNER!' : "It's a tie! Play again!" 
+    );
 }
 
 console.log("Hello World");
+game();
